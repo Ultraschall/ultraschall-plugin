5d194457050f4a683e82a2ce4e6339b96da9b0bf
Heiko Panjas <heiko@panjas.com>
2021-03-20 15:01:47 +0100

    Read release version from update server instead of pre-release version (win32)

--- src/UpdateHandler.cpp
+++ src/UpdateHandler.cpp
@@ -52,29 +52,23 @@ VERSION_TUPLE VERSION_TUPLE::ParseString(const UnicodeString& versionString)
 
     bool                     isValid = false;
     const UnicodeStringArray tokens  = UnicodeStringTokenize(versionString, '.');
-    if(tokens.size() > 1)
-    {
+    if(tokens.size() > 1) {
         isValid = TryEvaluateValue(tokens[0], MIN_MAJOR_VERSION, result.MAJOR);
-        if(true == isValid)
-        {
+        if(true == isValid) {
             isValid = TryEvaluateValue(tokens[1], MIN_MINOR_VERSION, result.MINOR);
-            if(true == isValid)
-            {
+            if(true == isValid) {
                 result.PATCH = MIN_PATCH_VERSION;
-                if(tokens.size() > 2)
-                {
+                if(tokens.size() > 2) {
                     isValid = TryEvaluateValue(tokens[2], MIN_PATCH_VERSION, result.PATCH);
                 }
-                else
-                {
+                else {
                     result.PATCH = MIN_PATCH_VERSION;
                 }
             }
         }
     }
 
-    if(false == isValid)
-    {
+    if(false == isValid) {
         result = VERSION_TUPLE();
     }
 
@@ -96,16 +90,13 @@ bool operator<(const VERSION_TUPLE& lhs, const VERSION_TUPLE& rhs)
 {
     bool isLess = false;
 
-    if(lhs.MAJOR < rhs.MAJOR)
-    {
+    if(lhs.MAJOR < rhs.MAJOR) {
         isLess = true;
     }
-    else if(lhs.MINOR < rhs.MINOR)
-    {
+    else if(lhs.MINOR < rhs.MINOR) {
         isLess = true;
     }
-    else if(lhs.PATCH < rhs.PATCH)
-    {
+    else if(lhs.PATCH < rhs.PATCH) {
         isLess = true;
     }
 
@@ -120,8 +111,7 @@ bool VERSION_TUPLE::TryEvaluateValue(const UnicodeString& valueString, const int
     value       = INVALID_VERSION;
 
     const int version = UnicodeStringToInt(valueString);
-    if(version >= minValue)
-    {
+    if(version >= minValue) {
         value  = version;
         result = true;
     }
@@ -134,14 +124,11 @@ bool UpdateHandler::IsUpdateCheckRequired()
     bool updateCheckRequired = false;
 
     if(ProfileProperty<bool>::Exists(
-           CHECK_ENABLED_PROFILE_NAME, CHECK_ENABLED_SECTION_NAME, CHECK_ENABLED_VALUE_NAME) == true)
-    {
+           CHECK_ENABLED_PROFILE_NAME, CHECK_ENABLED_SECTION_NAME, CHECK_ENABLED_VALUE_NAME) == true) {
         if(ProfileProperty<bool>::Query(
-               CHECK_ENABLED_PROFILE_NAME, CHECK_ENABLED_SECTION_NAME, CHECK_ENABLED_VALUE_NAME) == true)
-        {
+               CHECK_ENABLED_PROFILE_NAME, CHECK_ENABLED_SECTION_NAME, CHECK_ENABLED_VALUE_NAME) == true) {
             const double lastUpdateTimestamp = ReadLastUpdateTimestamp();
-            if(lastUpdateTimestamp > 0)
-            {
+            if(lastUpdateTimestamp > 0) {
                 const double now     = QueryCurrentTimeAsSeconds();
                 const double delta   = (now - lastUpdateTimestamp);
                 const double timeout = ONE_DAY_IN_SECONDS;
@@ -166,26 +153,20 @@ void UpdateHandler::Check()
 
     bool        messageDispatched = false;
     HttpClient* pClient           = new HttpClient();
-    if(pClient != nullptr)
-    {
+    if(pClient != nullptr) {
         bool                     downloadSucceeded = false;
         const UnicodeStringArray urls              = DownloadServerUrls();
-        for(size_t i = 0; (downloadSucceeded == false) && (i < urls.size()); i++)
-        {
+        for(size_t i = 0; (downloadSucceeded == false) && (i < urls.size()); i++) {
             const UnicodeString url                 = urls[i];
             UnicodeString       remoteVersionString = pClient->DownloadUrl(url);
-            if(remoteVersionString.empty() == false)
-            {
+            if(remoteVersionString.empty() == false) {
                 downloadSucceeded                 = true;
                 remoteVersionString               = SanitizeVersionString(remoteVersionString);
                 const VERSION_TUPLE remoteVersion = VERSION_TUPLE::ParseString(remoteVersionString);
-                if(VERSION_TUPLE::IsValid(remoteVersion) == true)
-                {
+                if(VERSION_TUPLE::IsValid(remoteVersion) == true) {
                     const VERSION_TUPLE localVersion = VERSION_TUPLE::ParseString(ULTRASCHALL_VERSION);
-                    if(VERSION_TUPLE::IsValid(localVersion) == true)
-                    {
-                        if(localVersion < remoteVersion)
-                        {
+                    if(VERSION_TUPLE::IsValid(localVersion) == true) {
+                        if(localVersion < remoteVersion) {
                             NotificationStore supervisor("ULTRASCHALL_UPDATE_CHECK");
                             UnicodeString     message = "An update for Ultraschall is available.\n\n";
                             message += "Go to https://ultraschall.fm/install to download the updated version ";
@@ -200,8 +181,7 @@ void UpdateHandler::Check()
 
         SafeRelease(pClient);
 
-        if(true == messageDispatched)
-        {
+        if(true == messageDispatched) {
             const double lastUpdateTimestamp = QueryCurrentTimeAsSeconds();
             WriteLastUpdateTimestamp(lastUpdateTimestamp);
         }
@@ -215,8 +195,7 @@ bool UpdateHandler::WriteLastUpdateTimestamp(const double timestamp)
     bool lastUpdateTimestampWritten = false;
 
     const UnicodeString checkpoint = std::to_string(timestamp);
-    if(checkpoint.empty() == false)
-    {
+    if(checkpoint.empty() == false) {
         ProfileProperty<UnicodeString>::Save(
             LAST_CHECKPOINT_PROFILE_NAME, LAST_CHECKPOINT_SECTION_NAME, LAST_CHECKPOINT_VALUE_NAME, checkpoint);
     }
@@ -230,14 +209,11 @@ double UpdateHandler::ReadLastUpdateTimestamp()
     double              lastUpdateTimestamp = INVALID_TIMESTAMP;
 
     if(ProfileProperty<UnicodeString>::Exists(
-           LAST_CHECKPOINT_PROFILE_NAME, LAST_CHECKPOINT_SECTION_NAME, LAST_CHECKPOINT_VALUE_NAME) == true)
-    {
+           LAST_CHECKPOINT_PROFILE_NAME, LAST_CHECKPOINT_SECTION_NAME, LAST_CHECKPOINT_VALUE_NAME) == true) {
         const UnicodeString previousUpdateCheckpoint = ProfileProperty<UnicodeString>::Query(
             LAST_CHECKPOINT_PROFILE_NAME, LAST_CHECKPOINT_SECTION_NAME, LAST_CHECKPOINT_VALUE_NAME);
-        if(previousUpdateCheckpoint.empty() == false)
-        {
-            try
-            {
+        if(previousUpdateCheckpoint.empty() == false) {
+            try {
                 lastUpdateTimestamp = std::stod(previousUpdateCheckpoint);
             }
             catch(std::invalid_argument&) // inconsistency in reaper-extstate.ini (number format), force update check
@@ -273,11 +249,7 @@ double UpdateHandler::QueryCurrentTimeAsSeconds()
 UnicodeStringArray UpdateHandler::DownloadServerUrls()
 {
     const UnicodeStringArray urls = {
-#ifndef __OPTIMIZE__
-        "https://ultraschall.io/current_prerelease.txt", "https://ultraschall.fm/current_prerelease.txt"};
-#else
         "https://ultraschall.io/current_release.txt", "https://ultraschall.fm/current_release.txt"};
-#endif
     return urls;
 }
 
@@ -289,8 +261,7 @@ UnicodeString UpdateHandler::SanitizeVersionString(const UnicodeString& versionS
     UnicodeStringTrim(result);
 
     const size_t position = result.find_first_of('\n');
-    if(position != UnicodeString::npos)
-    {
+    if(position != UnicodeString::npos) {
         result = versionString.substr(0, position);
     }
 
