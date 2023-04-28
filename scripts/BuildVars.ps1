2d26b782334d8e82b6aadba0007f4d818606b454
Heiko Panjas <heiko@panjas.com>
2023-04-28 16:14:44 +0200

    chore: Remove redundant files

--- scripts/BuildVars.ps1
+++
@@ -1,5 +0,0 @@
-
-$vsPath = & (Join-Path ${env:ProgramFiles(x86)} '\\Microsoft Visual Studio\\Installer\\vswhere.exe') -property installationPath
-Import-Module (Join-Path $vsPath 'Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll')
-# Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"
-Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments "-arch=x64" | Out-Null
